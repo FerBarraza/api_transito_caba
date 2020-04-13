c90f66b3f765964bbd613316342067ac154e7f3b
FerBarraza <fernandobarraza99@gmail.com>
2020-04-13 04:13:46 +0000

    en construccion

--- lib/api_elixir.ex
+++ lib/api_elixir.ex
@@ -2,7 +2,12 @@ defmodule ApiElixir do
 
 	def transito do
 		request("https://apitransporte.buenosaires.gob.ar/transito/v1/cortes?client_id=abbf905ff3d2490fbe7fa3f7a05c2e8b&client_secret=B9E5F51358344Ae5a338da50B9c250Ae")
-
+		|> body
+                |> parse_body_to_tuple
+                |> get_list_to_tuple
+                |> get_map_to_list
+                |> create_message
+                |> show_message	
 	end
 
 	defp request(url) do
@@ -14,19 +19,19 @@ defmodule ApiElixir do
 	end
 
 	defp parse_body_to_tuple(body) do
-		Poison.Parser.parse body
+		Poison.decode! body
 	end
 	
-	defp get_list_to_tuple(body) do
-		elem(tuple,1)
-	end
+#	defp get_list_to_tuple(body) do
+#		elem(tuple,1)
+#	end
 
-	defp get_map_to_list(list) do
-		list |> Enum.at 0
-	end
+#	defp get_map_to_list(list) do
+#		list |> Enum.at 0
+#	end
 
 	defp create_message(map) do
-		"> #{map}"
+#		"> #{map}"
 	end
 
 	defp show_message(message) do
